7d773e9f49f52b455ec7326056ad8ce19e593f13
Maarit Harsu <maarit.harsu@tuni.fi>
2021-08-31 16:23:07 +0300

    kierrokset 1-3 julkaistu / rounds 1-3 published

---
+++ README.md
@@ -0,0 +1,9 @@
+Ohjelmointi 2: Rakenteet / Programming 2: Structures
+
+Syksy/Autumn 2021
+
+Tämä repo sisältää opiskelijalle jaettavat materiaalit. /
+This repository contains materials for students.
+
+Kurssin materiaali / Course material:
+https://plus.tuni.fi/comp.cs.110/fall-2021/
---
+++ templates/02/grading/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+// Before passing automated tests, do not touch the values of the following
+// constants, even if they differ from those you see in Plussa.
+// At the end of the course, if you use this program to count your final grade,
+// you should update the constants to make them identical with those in Plussa.
+unsigned int MAX_N_POINTS = 780;
+unsigned int MAX_G_POINTS = 100;
+
+
+// Add functions if you feel it necessary
+
+int main()
+{
+    unsigned int n = 0;
+    unsigned int g = 0;
+    unsigned int p = 0;
+    unsigned int e = 0;
+    cout << "Enter N points, G points, P points: ";
+    cin >> n;
+    cin >> g;
+    cin >> p;
+    cout << "Enter exam grade (if no exam, enter zero): ";
+    cin >> e;
+
+    // Add more code
+
+    return 0;
+}
