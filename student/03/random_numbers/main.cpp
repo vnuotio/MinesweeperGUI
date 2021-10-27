0e92b0d3b7b5b00669231810d26bf138faefaf79
Vertti Nuotio <vertti.nuotio@tuni.fi>
2021-10-27 22:47:39 +0300

    random number gen loop

---
+++ student/03/random_numbers/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+void produce_random_numbers(unsigned int lower, unsigned int upper)
+{
+    int seed;
+    cout << "Enter a seed value: ";
+    cin >> seed;
+
+    default_random_engine gen(seed);
+    uniform_int_distribution<int> distr(lower, upper);
+
+    char cmd = ' ';
+    while (cmd != 'q')
+    {
+        cout << "Your drawn random number is " << distr(gen) << endl;
+        cout << "Press q to quit or any other key to continue: ";
+        cin >> cmd;
+        cout << endl;
+    }
+}
+
+int main()
+{
+    unsigned int lower_bound, upper_bound;
+    cout << "Enter a lower bound: ";
+    cin >> lower_bound;
+    cout << "Enter an upper bound: ";
+    cin >> upper_bound;
+
+    if(lower_bound >= upper_bound)
+    {
+        cout << "The upper bound must be strictly greater than the lower bound"
+             << endl;
+        return EXIT_FAILURE;
+    }
+
+    produce_random_numbers(lower_bound, upper_bound);
+
+    return EXIT_SUCCESS;
+}
