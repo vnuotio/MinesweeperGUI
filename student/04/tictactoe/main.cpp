2ff1a6f28ce717358f753e34e43da6d42f628fbf
Vertti Nuotio <vertti.nuotio@tuni.fi>
2021-11-08 01:58:03 +0200

    Syötteiden käsittely v1.

--- student/04/tictactoe/main.cpp
+++ student/04/tictactoe/main.cpp
@@ -35,7 +35,7 @@ Board initBoard()
     return newBoard;
 }
 
-void printBoard(Board board)
+void printBoard(const Board& board)
 {
     /* Tulostaa laudan konsoliin.
      * Parametrit:
@@ -65,11 +65,92 @@ void printBoard(Board board)
     cout << endl;
 }
 
+bool isNumeric(const string& str)
+{
+    return (all_of(str.begin(), str.end(), ::isdigit));
+}
+
+void expandBoard(bool bExpandRight, Board& board)
+{
+
+}
+
+bool takeInput(BoardMarks in_turn, Board& board)
+{
+    Board::size_type boardSize = board.size();
+    bool bInvalidInput = true;
+    string x;
+    string y;
+
+    while (bInvalidInput)
+    {
+        cout << "For " << (char)in_turn <<", enter coordinates: x y> ";
+        cin >> x;
+
+        // Quit-komento
+        if (x == "q" or x == "Q")
+        {
+            cout << "Why on earth you are giving up the game?" << endl;
+            return true;
+        }
+
+        cin >> y;
+
+        // Tyhjentää cin-bufferin, jolloin käyttäjän syöttämät ylimääräiset
+        // tiedot eivät jää häiritsemään myöhempiä suorituskertoja.
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        // Testaa, voiko syötteen muuntaa numeroiksi
+        if (not isNumeric(x) or not isNumeric(y))
+        {
+            cout << "Coordinate outside the board" << endl;
+            continue;
+        }
+
+        Board::size_type xInt = stoi(x);
+        Board::size_type yInt = stoi(y);
+
+        // Enemmän kuin 1 yli laitojen
+        if (xInt < 0 or yInt < 0 or xInt > boardSize+1 or yInt > boardSize+1)
+        {
+            cout << "Coordinate outside the board" << endl;
+            continue;
+        }
+        // Sekä yli että ali samaan aikaan
+        else if ((xInt == 0 and yInt == boardSize+1) or (xInt == boardSize+1 and yInt == 0))
+        {
+            cout << "Coordinate outside the board" << endl;
+            continue;
+        }
+    }
+    return false;
+}
+
 int main()
 {
     // Pelialueen luominen.
     Board board = initBoard();
 
-    printBoard(board);
-    return 0;
+    bool bGameOver = false;
+    BoardMarks in_turn;
+    int turn = 0;
+
+    cout << "Start game:" << endl;
+
+    while (not bGameOver)
+    {
+        if (turn % 2 == 0)
+        {
+            in_turn = CROSS;
+        }
+        else
+        {
+            in_turn = NOUGHT;
+        }
+
+        printBoard(board);
+        bGameOver = takeInput(in_turn, board);
+        ++turn;
+    }
+    return EXIT_SUCCESS;
 }
