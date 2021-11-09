7bb96dd04f066ef538b3329d18d04301d82668a7
Vertti Nuotio <vertti.nuotio@tuni.fi>
2021-11-09 22:08:10 +0200

    Tiedoston kuvaus lisätty main.ccp:n alkuun

--- student/04/tictactoe/main.cpp
+++ student/04/tictactoe/main.cpp
@@ -1,3 +1,31 @@
+/* Ristinolla
+ *
+ * Kuvaus:
+ *   Ohjelma toteuttaa ristinollapelin, jossa pelialuetta pystyy
+ * laajentamaan. Alussa peliruudukko on kooltaan 3 x 3. Peliruudukossa
+ * on kolmenlaisia merkkejä: risti (X), nolla (0) ja tyhjä (.).
+ *   Pelivuorossa ovat vuorotellen risti ja nolla, risti aloittaa.
+ * Joka kierroksella vuorossa olevalle merkille kysytään koordinaatteja
+ * eli kahta lukua. Vuorossa oleva merkki sijoitetaan peliruudukolla
+ * annettuihin koordinaatteihin, ja vuoro siirtyy toiselle merkille.
+ *   Pelin voittaa merkki, jolla saadaan ensimmäiseksi täytettyä peliruudukon
+ * kokonainen rivi, kokonainen sarake tai kokonainen diagonaali.
+ * Peli voi päättyä myös sellaiseen tilanteeseen, että peliruudukko on
+ * täynnä merkkejä, mutta kumpikaan merkki ei voittanut.
+ *   Ohjelma tarkistaa annetut koordinaatit. Koordinaattien pitää olla
+ * peliruudukon sisällä, tai koordinaatit saavat ylittyä tai alittua
+ * korkeintaan yhdellä. Jos annettu x- tai y-koordinaatti (rivi tai sarake)
+ * ylittyy yhdellä, peliruudukko laajenee oikealle ja alaspäin. Jos taas
+ * x- tai y-koordinaatti alittuu yhdelle, peliruudukko laajenee vasemmalle
+ * ja alaspäin. Peliruudukko siis pysyy aina neliön muotoisena.
+ *   Pelin päättyessä kerrotaan, kumpi voitti, vai voittiko kumpikaan.
+ *
+ * Ohjelman kirjoittaja:
+ * Nimi: Vertti Nuotio
+ * Opiskelijanumero: H300132
+ * Käyttäjätunnus: khvenu
+ * E-Mail: vertti.nuotio@tuni.fi
+ * */
 #include <iostream>
 #include <vector>
 #include <algorithm>
