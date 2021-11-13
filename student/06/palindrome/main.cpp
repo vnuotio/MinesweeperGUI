5b94495fcccd72428dc9b1f5fce204e61477b036
Vertti Nuotio <vertti.nuotio@tuni.fi>
2021-11-13 17:11:15 +0200

    Pitäis workkia

---
+++ student/06/palindrome/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#ifndef RECURSIVE_FUNC
+#define RECURSIVE_FUNC
+#endif
+
+bool palindrome_recursive(std::string s)
+{
+  RECURSIVE_FUNC
+  // Do not remove RECURSIVE_FUNC declaration, it's necessary for automatic testing to work
+  // ------------
+  if (s.length() <= 1)
+  {
+      return true;
+  }
+  else if (s.front() == s.back())
+  {
+      return palindrome_recursive(s.substr(1, s.length()-2));
+  }
+  else
+  {
+      return false;
+  }
+
+  // Add your implementation here
+}
+
+// Do not modify rest of the code, or the automated testing won't work.
+#ifndef UNIT_TESTING
+int main()
+{
+    std::cout << "Enter a word: ";
+    std::string word;
+    std::cin >> word;
+
+    if(palindrome_recursive(word)){
+        std::cout << word << " is a palindrome" << std::endl;
+    } else {
+        std::cout << word << " is not a palindrome" << std::endl;
+    }
+}
+#endif
