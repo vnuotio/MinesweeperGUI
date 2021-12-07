09d6b456f942510c0c6ea329ef43616276285cb6
Vertti Nuotio <vertti.nuotio@tuni.fi>
2021-12-07 20:43:12 +0200

    Add spinboxes

--- student/12/colorpicker_designer/mainwindow.cpp
+++ student/12/colorpicker_designer/mainwindow.cpp
@@ -17,6 +17,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->horizontalSliderBlue->setMinimum(0);
     ui->horizontalSliderBlue->setMaximum(RGB_VALUE_MAX);
 
+    ui->spinBoxRed->setMinimum(0);
+    ui->spinBoxRed->setMaximum(RGB_VALUE_MAX);
+
+    ui->spinBoxGreen->setMinimum(0);
+    ui->spinBoxGreen->setMaximum(RGB_VALUE_MAX);
+
+    ui->spinBoxBlue->setMinimum(0);
+    ui->spinBoxBlue->setMaximum(RGB_VALUE_MAX);
+
     connect(ui->horizontalSliderRed, &QSlider::valueChanged, this, &MainWindow::onColorChanged);
     connect(ui->horizontalSliderGreen, &QSlider::valueChanged, this, &MainWindow::onColorChanged);
     connect(ui->horizontalSliderBlue, &QSlider::valueChanged, this, &MainWindow::onColorChanged);
