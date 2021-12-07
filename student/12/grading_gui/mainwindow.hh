95468dfb5822f32763683125220781b50aea0f22
Vertti Nuotio <vertti.nuotio@tuni.fi>
2021-12-07 23:42:51 +0200

    V1 Done

--- student/12/grading_gui/mainwindow.hh
+++ student/12/grading_gui/mainwindow.hh
@@ -2,6 +2,7 @@
 #define MAINWINDOW_HH
 
 #include <QMainWindow>
+#include "gradecounter.hh"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +16,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_spinBoxN_valueChanged();
+    void on_spinBoxG_valueChanged();
+    void on_spinBoxP_valueChanged();
+    void on_spinBoxE_valueChanged();
+
+    void on_countPushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    unsigned int n = 0;
+    unsigned int g = 0;
+    unsigned int p = 0;
+    unsigned int e = 0;
 };
 #endif // MAINWINDOW_HH
